aa94dda5b036f5c4c7c479c344d15f6074c1daa3
hello <56610925+David373289@users.noreply.github.com>
2022-05-03 19:52:28 -0600

    Updated to use new query "on/off" instead of "yes/no"

--- scripts/check-icon.js
+++ scripts/check-icon.js
@@ -2,13 +2,13 @@ var darkIsOn = localStorage.getItem("dark-mode");
 var iconSet = document.querySelector("[settings]");
 
 if(!darkIsOn){
-    localStorage.setItem("dark-mode", "no");
+    localStorage.setItem("dark-mode", "off");
 }
 
 darkIsOn = localStorage.getItem("dark-mode");
 
-if(darkIsOn == "no"){
+if(darkIsOn == "off"){
     iconSet.src = "images/settings-icon.png";
-} else if(darkIsOn == "yes"){
+} else if(darkIsOn == "on"){
     iconSet.src = "images/settings-icon-dark.png";
 }
